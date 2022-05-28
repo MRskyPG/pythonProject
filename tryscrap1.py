6037c23cd33df462170339a7792b8cabe9ef78c8
Рогальский Михаил Владиславович <m.rogalskij.2021@stud.nstu.ru>
2022-05-28 12:34:36 +0700

    Checking info in index2.html with find_all

--- tryscrap1.py
+++ tryscrap1.py
@@ -1,3 +1,17 @@
+#pip install beautifulsoup4 lxml
+from bs4 import BeautifulSoup
+
 with open ("blank/index2.html") as file:
     src = file.read()
 
+soup = BeautifulSoup(src, "lxml")
+
+# title = soup.title
+# print(title.text)
+
+# .find(...) - the first object with name ... from (...) , but .find_all() - all objects with name ... adds to list.
+
+page_h1 = soup.find_all("h1")
+
+for item in page_h1:
+    print(item.text)
